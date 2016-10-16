999438b535c7db2fe188fd4f3a1ec69e2c9eec9e
S <deepa.s2@hpe.com>
2016-10-16 18:54:43 +0530

    initial comit for ds and algo

---
+++ Recursion/src/com/deepa/recusion/fibo/Runner.java
@@ -0,0 +1,34 @@
+package com.deepa.recusion.fibo;
+
+/**
+ * Recursion with memoization
+ * 
+ * @author ds8
+ *
+ */
+public class Runner {
+	private static int F[] = new int[51];// This is memoization i.e; We store
+											// the value of calculated fibos in
+											// this array.Hence made it global
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 51; i++) {
+			F[i] = -1;
+		}
+		F[0] = 0;
+		F[1] = 1;
+		System.out.println(Runner.fibo(10));
+	}
+
+	public static int fibo(int n) {
+
+		if (F[n] != -1) {
+			return F[n];// This means fibo is already calculated for that value
+						// of "n"
+		}
+
+		F[n] = fibo(n - 1) + fibo(n - 2);
+		return F[n];
+	}
+
+}
